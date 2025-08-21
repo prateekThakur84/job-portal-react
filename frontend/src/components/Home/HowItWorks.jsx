ece49d396c777936e0bad0f97ae01ee37a4561d8
Prateek Thakur <142097726+prateekThakur84@users.noreply.github.com>
2025-08-21 23:13:23 +0530

    minor changes

--- frontend/src/components/Home/HowItWorks.jsx
+++ frontend/src/components/Home/HowItWorks.jsx
@@ -8,30 +8,30 @@ const HowItWorks = () => {
     <>
       <div className="howitworks">
         <div className="container">
-          <h3>How Career Connect Works !</h3>
+          <h3>How Global Connect Works !</h3>
           <div className="banner">
             <div className="card">
               <FaUserPlus />
               <p>Create Account</p>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Consequuntur, culpa.
+                Sign up with your email and build your profile to showcase your
+                skills, experience, and preferences.
               </p>
             </div>
             <div className="card">
               <MdFindInPage />
               <p>Find a Job/Post a Job</p>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Consequuntur, culpa.
+                Browse thousands of job opportunities that match your profile or
+                post a job to find the right talent for your company.
               </p>
             </div>
             <div className="card">
               <IoMdSend />
               <p>Apply For Job/Recruit Suitable Candidates</p>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Consequuntur, culpa.
+                Apply directly to jobs that fit your career goals or connect with
+                qualified candidates to fill your open positions quickly.
               </p>
             </div>
           </div>
